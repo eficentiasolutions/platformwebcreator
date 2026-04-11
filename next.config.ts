bce14431d495c0a72bebaa845f8a403fbd7acbd6
Eficentia Solutions <dev@eficentiasolutions.com>
2026-04-11 00:16:27 +0000

    ae10c6fd-c64f-4117-a72e-38c847fc7452

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  allowedDevOrigins: [
+    "https://preview-web-c717a0ec-0d20-431f-8e99-0763aa1fe12a.space.chatglm.site",
+    "https://preview-chat-a4373f87-5452-4d2c-a436-e89101759d2a.space.z.ai",
+  ],
 };
 
 export default nextConfig;
